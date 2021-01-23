ef7e87a544a6e47324f1b83eb68df0ae7e797105
Constantinos Georgiou <constantgeorgiou@gmail.com>
2021-01-24 01:44:39 +0200

    back up

--- pomodoro.js
+++ pomodoro.js
@@ -1,9 +1,20 @@
+let workIntervalID; // used to start and stop the work count down
+let relaxIntervalID; // used to start and stop the break count down
 let timerIntervalID;
-let timer = 6000; // in milliseconds
-let remainingTime = timer; // in milliseconds
+
+let cycles;
+
+let work; // in milliseconds
+let relax; // in milliseconds
+let remainingTime; // in milliseconds
 
 function startTimer() {
-    timerIntervalID = setInterval(countDown, 1000);
+    if (cycles !== 0) {
+        timerIntervalID = setInterval(countDown, 1000);
+        workIntervalID = setTimeout();
+    } else {
+        cycles--;
+    }
 }
 
 function pauseTimer() {
@@ -12,8 +23,26 @@ function pauseTimer() {
 
 function resetTimer() {
     clearInterval(timerIntervalID);
-    remainingTime = timer;
-    updateTimer(calculateRemainingTime());
+
+    update(10, 0, 2, 0);
+}
+
+function updateTimer() {
+    update(
+        document.getElementById("settingsWorkMinutes").value,
+        document.getElementById("settingsWorkSeconds").value,
+        document.getElementById("settingsRelaxMinutes").value,
+        document.getElementById("settingsRelaxSeconds").value
+    );
+
+    console.log(
+        "UPDATED:\n work: " +
+            work +
+            "\n relax: " +
+            relax +
+            "\n remaining time: " +
+            remainingTime
+    );
 }
 
 function countDown() {
@@ -21,32 +50,22 @@ function countDown() {
     let minutes = Math.floor(remainingTime / 60000);
     let seconds = (remainingTime % 60000) / 1000;
 
-    console.log(minutes + ":" + seconds);
-
     if (seconds === 0 && minutes === 0) {
+        console.log("DONE!!!!");
         clearInterval(timerIntervalID);
-
-        alert("Done!");
     } else if (seconds === 0 && minutes !== 0) {
         minutes--;
         seconds = 59;
-        console.log("change: " + minutes + ":" + seconds);
     } else {
         seconds--;
     }
 
     // Update remaining time
-    remainingTime = minutes + seconds * 1000;
+    remainingTime = minutes * 60000 + seconds * 1000;
 
-    console.log("remaining: " + remainingTime + " " + minutes + ":" + seconds);
-
-    // Select elements in DOM
-    const minutesElement = document.getElementById("minutes");
-    const secondsElement = document.getElementById("seconds");
-
-    // Update elements in DOM with remaining time
-    minutesElement.value = minutes;
-    secondsElement.value = seconds;
+    // Select elements and update DOM elements with remaining time:
+    const minutesElement = document.getElementById("minutes").textContent = minutes;
+    const secondsElement = document.getElementById("seconds").textContent = seconds;
     document.title = "Pomodoro: " + minutes + ":" + seconds;
 }
 
@@ -62,3 +81,37 @@ function updateButtons() {
         pauseElement.classList.add("hide");
     }
 }
+
+// Initializes DOM elements and work and relax timer:
+function update(
+    workMinutes,
+    workSeconds,
+    relaxMinutes,
+    relaxSeconds,
+    numberOfCycles
+) {
+    console.log(
+        "wm: " +
+            workMinutes +
+            " ws: " +
+            workSeconds +
+            " rm: " +
+            relaxMinutes +
+            " rs: " +
+            relaxSeconds
+    );
+    document.getElementById("minutes").textContent = workMinutes || 25;
+    document.getElementById("seconds").textContent = workSeconds || 00;
+    // document.getElementById("minutes").value = workMinutes || 25;
+    // document.getElementById("seconds").value = workSeconds || 00;
+
+    cycles = numberOfCycles || 4;
+
+    work = (workMinutes || 25) * 60000 + (workSeconds || 0) * 1000;
+    relax = (relaxMinutes || 25) * 60000 + (relaxSeconds || 0) * 1000;
+
+    remainingTime = work;
+}
+
+// Initialize text inputs minutes and seconds on page load:
+window.onload = update(0, 3, 0, 5, 4);
